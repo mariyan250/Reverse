4f16d7b0448d7a059bd4a9548980a8e94ded513e
mariyan250 <mariyan250@gmail.com>
2020-07-02 15:24:45 +0300

    Initial commit.

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className={style.title}>Welcome</h1>
+    </div>
+  );
+};
+
+export default App;
