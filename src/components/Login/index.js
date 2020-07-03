be57a56b9f5a766ae71c8db6c70baab62bf6a59e
mariyan250 <mariyan250@gmail.com>
2020-07-03 15:19:37 +0300

    Added Register component.

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -9,6 +9,11 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleLogin = (e) => {
+    e.preventDefault();
+    alert('Login!');
+  }
+
   return (
     <main className="login">
       <section className="login-intro">
@@ -17,20 +22,25 @@ const Login = () => {
           <h3>Sell your items now!</h3>
         </section>
       </section>
+
       <aside className="aside">
-        <form action="/login" method="POST">
+        <form onSubmit={handleLogin}>
           <h2>Sign In</h2>
+
           <div className="row">
             <label htmlFor="email">Email</label>
             <input type="text" name="email" value={email}
-              onChange={(e) => setEmail(e.target.value)} />
+              onChange={(e) => setEmail(e.target.value)} required />
           </div>
+
           <div className="row">
-            <label htmlFor="email">Password</label>
-            <input type="password" name="Password" value={password}
-              onChange={(e) => setPassword(e.target.value)} />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" value={password}
+              onChange={(e) => setPassword(e.target.value)} required />
           </div>
+
           <button className="auth-button">Submit</button>
+
           <Link to="/register" className="register-btn">Don't have an account?</Link>
         </form>
       </aside>
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,11 +1,54 @@
 // React and Style
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
+// Router
+import { Link } from 'react-router-dom';
+
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rePassword, setRePassword] = useState('');
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    alert('Register!');
+  }
+
   return (
-    <main className="login">
-      <h1>Register!</h1>
+    <main className="register">
+      <form onSubmit={handleRegister}>
+        <h2>Sign Up</h2>
+
+        <div className="row">
+          <label htmlFor="name">Full Name</label>
+          <input type="text" name="name" value={name}
+            onChange={(e) => setName(e.target.value)} required />
+        </div>
+
+        <div className="row">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" value={email}
+            onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+
+        <div className="row">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" value={password}
+            onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+
+        <div className="row">
+          <label htmlFor="rePassword">Repeat Password</label>
+          <input type="password" name="rePassword" value={rePassword}
+            onChange={(e) => setRePassword(e.target.value)} required />
+        </div>
+
+        <button className="auth-button">Submit</button>
+
+        <Link to="/" className="register-btn">Already have an account?</Link>
+      </form>
     </main>
   );
 };
