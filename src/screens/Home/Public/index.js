cf27c898c7d0bb906fcaae299bfc01840d75d93f
mariyan250 <mariyan250@gmail.com>
2020-07-10 14:42:34 +0300

    Changed Home Screen.

--- src/screens/Home/Public/index.js
+++ src/screens/Home/Public/index.js
@@ -20,7 +20,29 @@ const Home = () => {
         </section>
       </section>
 
-      <section className='two py-0 py-md-5'>
+      <section className='one py-5'>
+        <hr className='mb-5 mx-auto' />
+        <article className='d-md-flex flex-column px-4'>
+          <h2 className='mt-2 mb-5 text-center'>Description</h2>
+          <article className='d-flex flex-column flex-lg-row justify-content-between'>
+            <p className='my-5 mx-4 my-lg-0'>
+              Reverse is not just a platform for selling items with ease, but
+              also a social network for people with same interests and needs.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              aut fuga ipsam! Consectetur illum, ex odit alias dignissimos sint
+              ea impedit beatae vitae asperiores quos? Autem nesciunt odit vero
+              eos!
+            </p>
+            <img
+              src='/img/cloths.jpg'
+              alt=''
+              className='img-fluid align-self-center'
+            />
+          </article>
+        </article>
+      </section>
+
+      <section className='two py-0 bg-primary'>
         <article className='py-3 py-md-5 d-flex flex-column flex-md-row align-items-center align-items-md-start justify-content-around vh-100'>
           <Card iconName='fas fa-check' text='Sell your items.' />
           <Card iconName='fas fa-truck' text='Ship your products.' />
