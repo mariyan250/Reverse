4d747292142e65efc402f5ec9bb994616cdc1b0b
mariyan250 <mariyan250@gmail.com>
2020-07-13 23:30:50 +0300

    Changed Private Home Screen.

--- src/screens/Home/Private/index.js
+++ src/screens/Home/Private/index.js
@@ -14,7 +14,7 @@ const HomePrivate = () => {
       <Header />
       <form className="search mt-5 container-fluid px-1">
         <div className="row mx-auto justify-content-center">
-          <div className="col-10 col-md-3 p-0 d-flex">
+          <div className="search-col col-11 col-md-3 d-flex">
             <input
               type="text"
               name="search"
@@ -33,7 +33,7 @@ const HomePrivate = () => {
       </form>
 
       <section className="home-one mt-4 px-4">
-        <h2 className="my-5 mx-2">Category</h2>
+        <h2 className="mt-5 mb-4 mx-2">Category</h2>
         <article className="container-fluid justify-content-center align-items-center">
           <div className="row">
             <div className="col-6 p-2 col-sm-3 col-md-2 p-md-2">
