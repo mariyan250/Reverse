51d2f41702ca495fb6986365d92ec21078410088
mariyan250 <mariyan250@gmail.com>
2020-07-13 13:39:53 +0300

    Added Header, Nav Links and Menu.

--- src/shared/components/Header/index.js
+++ src/shared/components/Header/index.js
@@ -2,20 +2,57 @@
 import React, { useState } from 'react';
 import './index.scss';
 
+// Router
+import { NavLink } from 'react-router-dom';
+
 // Components
 import Hamburger from 'shared/components/Hamburger';
+import Menu from 'shared/components/Menu';
 
 function Header() {
   const [burgerOpened, setBurgerOpened] = useState(false);
 
   return (
-    <div className="header px-4 py-3 d-flex justify-content-between align-items-center">
+    <section className="header px-4 py-3 d-flex justify-content-between align-items-center">
       <h1>Reverse</h1>
+
+      <ul className="d-none d-md-flex h-100 align-items-center m-0">
+        <li className="ml-5">
+          <NavLink to="/dashboard" className="link" activeClassName="active-link">
+            <i className="fas fa-columns mr-3" />
+            Dashboard
+          </NavLink>
+        </li>
+
+        <li className="ml-5">
+          <NavLink to="/login" className="link" activeClassName="active-link">
+            <i className="fas fa-folder mr-3" />
+            My Items
+          </NavLink>
+        </li>
+
+        <li className="ml-5">
+          <NavLink to="/register" className="link" activeClassName="active-link">
+            <i className="fas fa-user mr-3" />
+            Friends
+          </NavLink>
+        </li>
+
+        <li className="ml-5">
+          <NavLink to="/register" className="link" activeClassName="active-link">
+            <i className="fas fa-cog mr-3" />
+            Settings
+          </NavLink>
+        </li>
+      </ul>
+
       <Hamburger
-        className={`hamburger ${burgerOpened ? 'toggled' : ''}`}
+        className={`hamburger d-md-none ${burgerOpened ? 'toggled' : ''}`}
         onClick={() => setBurgerOpened(!burgerOpened)}
       />
-    </div>
+
+      {burgerOpened && <Menu className="header-menu position-fixed d-md-none" />}
+    </section>
   );
 }
 
---
+++ src/shared/components/Menu/index.js
@@ -0,0 +1,62 @@
+// React and Style
+import React from 'react';
+import './index.scss';
+
+// Router
+import { NavLink } from 'react-router-dom';
+
+function Menu(props) {
+  return (
+    <section {...props}>
+      <article className="menu">
+        <ul className="d-flex flex-column w-100 h-100">
+          <li className="d-flex">
+            <NavLink
+              to="/dashboard"
+              className="menu-link d-flex justify-content-center align-items-center text-decoration-none"
+              activeClassName="link-active"
+            >
+              <i className="fas fa-columns mr-3" />
+              Dashboard
+            </NavLink>
+          </li>
+
+          <li className="d-flex">
+            <NavLink
+              to="/login"
+              className="menu-link d-flex justify-content-center align-items-center text-decoration-none"
+              activeClassName="link-active"
+            >
+              <i className="fas fa-folder mr-3" />
+              My Items
+            </NavLink>
+          </li>
+
+          <li className="d-flex">
+            <NavLink
+              to="/register"
+              className="menu-link d-flex justify-content-center align-items-center text-decoration-none"
+              activeClassName="link-active"
+            >
+              <i className="fas fa-user mr-3" />
+              Friends
+            </NavLink>
+          </li>
+
+          <li className="d-flex">
+            <NavLink
+              to="/register"
+              className="menu-link d-flex justify-content-center align-items-center text-decoration-none"
+              activeClassName="link-active"
+            >
+              <i className="fas fa-cog mr-3" />
+              Settings
+            </NavLink>
+          </li>
+        </ul>
+      </article>
+    </section>
+  );
+}
+
+export default Menu;
