3e4b229c217df581e19c02a5a76742f50e2891ce
mariyan250 <mariyan250@gmail.com>
2020-07-15 13:11:25 +0300

    Refactored styles and components

--- src/screens/Search/components/ItemList/index.js
+++ src/screens/Search/components/ItemList/index.js
@@ -2,14 +2,18 @@
 import React from 'react';
 import './index.scss';
 
+// Translations
+import { useTranslation } from 'react-i18next';
+
 // Components
 import Item from '../Item';
 
-// translations?
 function ItemList({ items }) {
+  const { t } = useTranslation();
+
   return (
     <section className="item-list container-fluid px-4">
-      <h2>Items</h2>
+      <h2>{t('search_screen.heading')}</h2>
       <ul className="row pt-5 pb-1">
         {items && items.map((item) => <Item {...item} key={item.id} />)}
       </ul>
