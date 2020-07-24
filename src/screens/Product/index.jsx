a0e488c2b0701791185629f28ff8f128456b26f8
mariyan250 <mariyan250@gmail.com>
2020-07-24 19:42:45 +0300

    Changed Product Screen.

--- src/screens/Product/index.jsx
+++ src/screens/Product/index.jsx
@@ -6,6 +6,9 @@ import './index.scss';
 import { connect } from 'react-redux';
 import { getPost } from 'store/actions/post';
 
+// Translations
+import { useTranslation } from 'react-i18next';
+
 // Image Gallery
 import ImageGallery from 'react-image-gallery';
 
@@ -14,6 +17,8 @@ import Header from 'shared/components/Header';
 import NotFound from 'screens/NotFound';
 
 function Product({ getPost, post, match }) {
+  const { t } = useTranslation();
+
   useEffect(() => {
     if (match.params) {
       getPost(match.params.id);
@@ -37,7 +42,7 @@ function Product({ getPost, post, match }) {
     <Fragment>
       <Header />
       <section className="item-screen p-lg-4">
-        <article className="gallery">
+        <article className="gallery px-3">
           {post.images && (
             <ImageGallery
               items={getImages(post)}
@@ -49,9 +54,9 @@ function Product({ getPost, post, match }) {
           )}
         </article>
 
-        <article className="item-description p-4 p-lg-5">
-          <h1 className="mb-3 app-text-primary">{post.name}</h1>
-          <p className="mb-3">
+        <article className="item-description p-4 pb-5 p-lg-5">
+          <h1 className="mb-3 mt-4 mt-md-0">{post.name}</h1>
+          <p className="mb-3 mt-4">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem voluptatibus, laborum
             ut aliquid optio architecto quos libero sint odit earum fugiat alias. Consequuntur quam
             eaque, dolorum necessitatibus quisquam iste, harum pariatur quos qui placeat nam maiores
@@ -59,10 +64,30 @@ function Product({ getPost, post, match }) {
             maxime molestias sequi?
           </p>
 
-          <section className="item-description-nav d-flex justify-content-between align-items-center mt-4">
-            <h1 className="app-text-pr">{post.price}$</h1>
-            <span className="app-bg-primary app-text-white px-5 py-3 px-lg-4 py-lg-2 rounded">
-              Call Now
+          <section className="item-description-second d-md-flex justify-content-between my-4">
+            <p>
+              <i className="fas fa-map-marker-alt mr-3" />
+              {post.location}
+            </p>
+
+            <p>
+              <i className="fas fa-clock mr-3" />
+              {post.date}
+            </p>
+
+            <p className="mb-5">
+              <i className="fas fa-user mr-3" />
+              {post.seller}
+            </p>
+          </section>
+
+          <section className="item-description-nav app-bg-white d-flex justify-content-around justify-content-sm-between align-items-center py-1 m-sm-0 mt-4 w-100">
+            <h3 className="text-center my-2 my-md-0">
+              {post.price} {t('general.currency')}
+            </h3>
+            <span className="app-bg-primary app-text-white rounded py-3 px-5 px-md-4 py-md-2">
+              {t('product.call_button')}
+              <i className="fas fa-phone ml-3" />
             </span>
           </section>
         </article>
