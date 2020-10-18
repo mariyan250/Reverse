5e02b941bf2031612bc4690b08b8b8509602559d
mariyan250 <mariyan250@gmail.com>
2020-10-18 17:39:47 +0300

    Changed items card style.

--- src/components/Product/Card/index.jsx
+++ src/components/Product/Card/index.jsx
@@ -12,7 +12,7 @@ function Item({ id, images, name, location, date, price }) {
   const { t } = useTranslation();
 
   return (
-    <li className={`${styles.item} app-bg-white overflow-hidden`}>
+    <li className={`${styles.item} app-bg-white overflow-hidden rounded-lg`}>
       <Link to={`/item/${id}`} className="app-text-secondary">
         <div className={`${styles.container} d-flex position-relative justify-content-center`}>
           <img
@@ -22,22 +22,24 @@ function Item({ id, images, name, location, date, price }) {
           />
         </div>
 
-        <div className="py-3 px-4 h-100 app-bg-gray">
-          <p className="mb-4">{name}</p>
+        <div className="p-2 h-100">
+          <div className="p-3 app-bg-gray-darkest rounded-lg">
+            <p className={`${styles['item-name']} mb-2`}>{name}</p>
 
-          <p className="mb-3">
-            <i className="fas fa-map-marker-alt mr-2" />
-            {location}
-          </p>
+            <div className={`${styles['item-stars']} mb-3`}>
+              <i className="fas fa-star"></i>
+            </div>
 
-          <p className="mb-4">
-            <i className="fas fa-clock mr-2" />
-            {date}
-          </p>
+            <div className="d-flex justify-content-between align-items-center">
+              <p class={`${styles['item-price']} app-text-primary`}>
+                {price} {t('general.currency')}
+              </p>
 
-          <p>
-            {price} {t('general.currency')}
-          </p>
+              <button className="border-0 py-3 px-4 py-md-2 px-md-3 app-text-white rounded-lg app-bg-primary">
+                Add to cart
+              </button>
+            </div>
+          </div>
         </div>
       </Link>
     </li>
